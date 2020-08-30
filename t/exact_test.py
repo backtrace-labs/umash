a4fe35a5039bd98a013d96643529a655f9b4b548
Paul Khuong <pvk@pvk.ca>
2020-08-30 19:24:08 -0400

    NFC t/exact_test: factor out the statistic computation code
    Having a generator for the stream of resampled results will be
    particularly useful for multi-threaded resampling.
    
    TESTED=smoke test pasted at the bottom of `exact_test.py`.

--- t/exact_test.py
+++ t/exact_test.py
@@ -1,8 +1,42 @@
 import math
-import struct
 from csm import csm
 from umash import BENCH
 from umash import BENCH_FFI as FFI
+
+
+def _exact_test_result(buf, m, n, stat_fn):
+    """Computes the actual sample value for `stat_fn`, for the sample
+    values in `buf`, with `m` values from class A followed by `n` from
+    class B.
+    """
+    total = m + n
+    copy = FFI.new("uint64_t[]", total)
+    xoshiro = BENCH.exact_test_prng_create()
+    try:
+        FFI.memmove(copy, buf, total * FFI.sizeof("uint64_t"))
+        BENCH.exact_test_offset_sort(xoshiro, copy, m, n, 0, 0)
+        return stat_fn(copy, m, n)
+    finally:
+        BENCH.exact_test_prng_destroy(xoshiro)
+
+
+def _resample_exact_test_results(buf, m, n, stat_fn, p_a_lt, a_offset, b_offset):
+    """Yields values computed by `stat_fn` after shuffling (copies of)
+    `buf`, with `m` values from class A and `n` from class B.
+    """
+    total = m + n
+    copy = FFI.new("uint64_t[]", total)
+    error_ptr = FFI.new("char**")
+    xoshiro = BENCH.exact_test_prng_create()
+    try:
+        while True:
+            FFI.memmove(copy, buf, total * FFI.sizeof("uint64_t"))
+            if not BENCH.exact_test_shuffle(xoshiro, copy, m, n, p_a_lt, error_ptr):
+                raise "Shuffle failed: %s" % str(error_ptr[0], "utf-8")
+            BENCH.exact_test_offset_sort(xoshiro, copy, m, n, a_offset, b_offset)
+            yield stat_fn(copy, m, n)
+    finally:
+        BENCH.exact_test_prng_destroy(xoshiro)
 
 
 def exact_test(a, b, statistic="lte", eps=1e-6, p_a_lt=0.5, a_offset=0, b_offset=0):
@@ -47,7 +81,6 @@ def exact_test(a, b, statistic="lte", eps=1e-6, p_a_lt=0.5, a_offset=0, b_offset
 
     m = len(a)
     n = len(b)
-    total = m + n
 
     # Apply a fudged Bonferroni correction for the two-sided quantile
     # test.
@@ -61,64 +94,68 @@ def exact_test(a, b, statistic="lte", eps=1e-6, p_a_lt=0.5, a_offset=0, b_offset
     gte_actual = 0
 
     test_every = 10
-    copy = FFI.new("uint64_t[]", total)
-    error_ptr = FFI.new("char**")
     buf = _make_buf()
-    xoshiro = BENCH.exact_test_prng_create()
-    try:
-        FFI.memmove(copy, buf, total * FFI.sizeof("uint64_t"))
-        BENCH.exact_test_offset_sort(xoshiro, copy, m, n, 0, 0)
-        actual_stat = stat_fn(copy, m, n)
-        print("actual: %f" % actual_stat)
+    actual_stat = _exact_test_result(buf, m, n, stat_fn)
+    print("actual: %f" % actual_stat)
 
-        while True:
-            FFI.memmove(copy, buf, total * FFI.sizeof("uint64_t"))
-            if not BENCH.exact_test_shuffle(xoshiro, copy, m, n, p_a_lt, error_ptr):
-                raise "Shuffle failed: %s" % str(error_ptr[0], "utf-8")
-            BENCH.exact_test_offset_sort(xoshiro, copy, m, n, a_offset, b_offset)
-            stat = stat_fn(copy, m, n)
-            if stat <= actual_stat:
-                lte_actual += 1
-            if stat >= actual_stat:
-                gte_actual += 1
-            trials += 1
+    for stat in _resample_exact_test_results(
+        buf, m, n, stat_fn, p_a_lt, a_offset, b_offset
+    ):
+        if stat <= actual_stat:
+            lte_actual += 1
+        if stat >= actual_stat:
+            gte_actual += 1
+        trials += 1
 
-            if (trials % test_every) != 0:
-                continue
+        if (trials % test_every) != 0:
+            continue
 
-            if trials >= 100 * test_every:
-                test_every *= 10
-            count_in_middle = 0
-            lt_significant, lt_level = csm(trials, eps, lte_actual, log_inner_eps)
-            gt_significant, gt_level = csm(trials, eps, gte_actual, log_inner_eps)
+        if trials >= 100 * test_every:
+            test_every *= 10
+        count_in_middle = 0
+        lt_significant, lt_level = csm(trials, eps, lte_actual, log_inner_eps)
+        gt_significant, gt_level = csm(trials, eps, gte_actual, log_inner_eps)
 
-            print(
-                "%i: %i %i (%f %f / %f)"
-                % (
-                    trials,
-                    lte_actual,
-                    gte_actual,
-                    max(lt_level, gt_level),
-                    stat,
-                    actual_stat,
-                )
+        print(
+            "%i: %i %i (%f %f / %f)"
+            % (
+                trials,
+                lte_actual,
+                gte_actual,
+                max(lt_level, gt_level),
+                stat,
+                actual_stat,
             )
-            if lt_significant:
-                # We're pretty sure the actual stat is too low to
-                # realistically happen under then null
-                if lte_actual / trials < eps:
-                    return -1, trials
-                count_in_middle += 1
+        )
+        if lt_significant:
+            # We're pretty sure the actual stat is too low to
+            # realistically happen under then null
+            if lte_actual / trials < eps:
+                return -1, trials
+            count_in_middle += 1
 
-            if gt_significant:
-                # We're pretty sure the actual stat is too high.
-                if gte_actual / trials < eps:
-                    return 1, trials
-                count_in_middle += 1
+        if gt_significant:
+            # We're pretty sure the actual stat is too high.
+            if gte_actual / trials < eps:
+                return 1, trials
+            count_in_middle += 1
 
-            if count_in_middle == 2:
-                # We're sure the actual stat isn't too low nor too
-                # high for the null.
-                return 0, trials
-    finally:
-        BENCH.exact_test_prng_destroy(xoshiro)
+        if count_in_middle == 2:
+            # We're sure the actual stat isn't too low nor too
+            # high for the null.
+            return 0, trials
+
+
+# In [1]: import random
+# In [2]: import math
+# In [3]: import exact_test
+# In [4]: A = [max(0, math.ceil(random.normalvariate(1000, 10))) for _ in range(10000)]
+# In [5]: B = [max(0, math.ceil(random.normalvariate(1001, 10))) for _ in range(10000)]
+# In [6]: exact_test.exact_test(A, B, eps=1e-4)
+# actual: 0.543088
+# 10: 10 0 (2.397441 0.508931 / 0.543088)
+# 20: 20 0 (3.043613 0.502342 / 0.543088)
+# ...
+# 560000: 560000 0 (-12.219430 0.504535 / 0.543088)
+# 570000: 570000 0 (-12.656286 0.513219 / 0.543088)
+# Out[6]: (1, 570000)
