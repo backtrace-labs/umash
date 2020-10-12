8fd6287617f41e236bfb679e8d29a8b32f82c0e9
Paul Khuong <pvk@pvk.ca>
2020-10-12 19:57:26 -0400

    t/test_umash_long: make sure to test long (>= 512) repeated strings
    This will catch simple issues in the block compressor.
    
    TESTED=it's test code.

--- t/test_umash_long.py
+++ t/test_umash_long.py
@@ -46,3 +46,29 @@ def test_umash_long(seed, multiplier, key, data):
         params[0].oh[i] = param
 
     assert C.umash_long(poly, params[0].oh, seed, block, n_bytes) == expected
+
+
+@given(
+    seed=U64S,
+    multiplier=st.integers(min_value=0, max_value=FIELD - 1),
+    key=st.lists(
+        U64S, min_size=C.UMASH_OH_PARAM_COUNT, max_size=C.UMASH_OH_PARAM_COUNT
+    ),
+    data=repeats(512),
+)
+def test_umash_long_repeat(seed, multiplier, key, data):
+    """Compare umash_long on repeated strings with the reference."""
+    expected = umash(UmashKey(poly=multiplier, oh=key), seed, data)
+    note(len(data))
+
+    n_bytes = len(data)
+    block = FFI.new("char[]", n_bytes)
+    FFI.memmove(block, data, n_bytes)
+    poly = FFI.new("uint64_t[2]")
+    poly[0] = (multiplier ** 2) % FIELD
+    poly[1] = multiplier
+    params = FFI.new("struct umash_params[1]")
+    for i, param in enumerate(key):
+        params[0].oh[i] = param
+
+    assert C.umash_long(poly, params[0].oh, seed, block, n_bytes) == expected
